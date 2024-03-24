a99dd228ca6ba9f91ec9c63be69ea7efa5aebd86
ManuelE-Osorio <aux.manuel.osorio@gmail.com>
2024-03-24 15:46:25 -0500

    Styling

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ASP.NET MVC Todo List app using Minimal and Fetch API
 
+## Todo
+
+1. Refactor api to use Typed Results
+2. Prevent Over Posting
+3. Add Map groups
+
 ## References
 
 1. <https://learn.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-8.0&tabs=visual-studio>
@@ -9,3 +15,4 @@ ASP.NET MVC Todo List app using Minimal and Fetch API
 3. <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise>
 4. <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises>
 5. <https://learn.microsoft.com/en-us/aspnet/core/fundamentals/apis?view=aspnetcore-8.0>
+6. <https://learn.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-8.0#ecors6>
--- TodoListWebPage/js/fetch.js
+++ TodoListWebPage/js/fetch.js
@@ -1,5 +1,18 @@
 window.onload = getTodos()
 
+document.getElementById('todoModal').addEventListener('show.bs.modal', event => createModal(event))
+document.getElementById('todoModal').querySelector('form').addEventListener('submit', handleForm)
+
+
+function handleForm(e)
+{
+    e.preventDefault()
+    const form = e.target
+    const formData = new FormData(form)
+    const todo = Object.fromEntries(formData);
+    console.log(todo)
+}
+
 function getTodos()
 {
     const apiAddress = 'https://localhost:7048/todolist'
@@ -11,7 +24,7 @@ function getTodos()
             console.log(element)
             html += generateItem(element)
         });
-        document.getElementById('accordionExample').innerHTML = html
+        document.getElementById('todoAccordion').innerHTML = html
         })
     .catch( e => console.log(e))
 }
@@ -50,30 +63,66 @@ function generateItem(obj)
             <div class="col">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${obj.id}" aria-expanded="false" aria-controls="collapse${obj.id}">
                     <div class="col">
-                        <strong>Title: </strong> ${obj.title}
+                        <strong>Title: </strong>
+                        <label id="todotitle${obj.id}">${obj.title}</label>
                     </div>
                     <div class="col">
-                        <strong>Status:</strong> ${obj.status}
+                        <strong>Status:</strong>
+                        <label id="todostatus${obj.id}">${obj.status}</label>
                     </div>                
                 </button>
             </div>
             <div class="col-1">
-                <div class="col1 btn btn-primary" onclick="updateTodo(${obj.id})"> Update</div>
-            </div>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#todoModal" data-bs-id="${obj.id}" data-bs-title="Update">Update</button>
+        </div>
             <div class="col-1">
                 <div class="col1 btn btn-danger" onclick="deleteTodo(${obj.id})"> Delete</div>
             </div>
         </div>
     </h2>
-    <div id="collapse${obj.id}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+    <div id="collapse${obj.id}" class="accordion-collapse collapse" data-bs-parent="#todoAccordion">
         <div class="accordion-body">
-        <strong>Description:</strong> ${obj.description}
+        <strong>Description:</strong>
+        <label id="tododescription${obj.id}">${obj.description}</label>
         </div>
     </div>
     </div>`
 }
 
-function generateForm(obj)
+function createModal(event)
 {
-    return ``
+    const exampleModal = document.getElementById('todoModal')
+    const id = event.relatedTarget.getAttribute('data-bs-id')
+    const title = event.relatedTarget.getAttribute('data-bs-title')
+
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalid = exampleModal.querySelector('#todoid')
+    const modalTodoTitle = exampleModal.querySelector('#inputTitle')
+    const modalStatus = exampleModal.querySelector('#inputStatus')
+    const modalTodoDesc = exampleModal.querySelector('#inputTextArea')
+    const modalButton = exampleModal.querySelector('#inputButton')
+
+    modalTitle.textContent = `${title} Todo Item ${id}`  //delete id
+    modalid.setAttribute('value', `${id}`)
+
+    if(id > 0)
+    {
+        const todoItem = document.getElementById(`accordion${id}`)
+        const todoTitle = todoItem.querySelector(`#todotitle${id}`).innerHTML
+        const todoStatus = todoItem.querySelector(`#todostatus${id}`).innerHTML  //pending set status
+        const todoDescription = todoItem.querySelector(`#tododescription${id}`).innerHTML
+
+        
+        modalTodoTitle.setAttribute('value', `${todoTitle}`)
+        modalStatus.value = todoStatus
+        modalTodoDesc.value = todoDescription  //pending change button to update
+        modalButton.innerHTML = 'Update'
+    }
+    else
+    {
+        modalTodoTitle.setAttribute('value', '')
+        modalTodoDesc.value = ''
+        modalButton.innerHTML = 'Create'
+    }
+
 }
